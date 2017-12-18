c296f199fabf048c294395808d6ae63b3856e5e2
Martin Gomez <martyngomez22@gmail.com>
2017-12-18 17:28:23 -0300

    Create Account Layout created.

--- app/src/main/java/com/mardev/platzigram/view/CreateAccountActivity.java
+++ app/src/main/java/com/mardev/platzigram/view/CreateAccountActivity.java
@@ -2,6 +2,7 @@ package com.mardev.platzigram.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.mardev.platzigram.R;
 
@@ -11,5 +12,14 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolbar(getResources().getString(R.string.toolbar_title_createaccount),false);//Linkea con el recurso
+    }
+
+
+    public void showToolbar(String title, boolean upButton ) {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
