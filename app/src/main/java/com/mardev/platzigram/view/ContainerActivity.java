d9e7e9eebde6f7da23a7f4d1d87a58d53a9da935
Martin Gomez <martyngomez22@gmail.com>
2017-12-26 12:42:27 -0300

    Container Fragments added.

---
+++ app/src/main/java/com/mardev/platzigram/view/ContainerActivity.java
@@ -0,0 +1,15 @@
+package com.mardev.platzigram.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mardev.platzigram.R;
+
+public class ContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container);
+    }
+}
