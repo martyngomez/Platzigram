551ee6455fb013935e180cd98636242261719de0
Martin Gomez <martyngomez22@gmail.com>
2018-01-08 16:41:49 -0300

    External URL added at logo.

--- app/src/main/java/com/mardev/platzigram/LoginActivity.java
+++ app/src/main/java/com/mardev/platzigram/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.mardev.platzigram;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,6 +15,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Otra forma de hacer un intent implicito
+       /* ImageView logo = (ImageView) findViewById(R.id.logo);
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://platzigram.com/"));
+                startActivity(intent);
+            }
+        });*/
     }
 
     public void goCreateAccount(View view){
@@ -25,4 +36,16 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ContainerActivity.class); //Prmer parametro :actividad origen, seundo actividad destino
         startActivity(intent);
     }
+
+    //Deesafio 2
+    public void goWeb(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("http://www.google.com"));
+        startActivity(intent);
+    }
+
+
+
+
+
 }
