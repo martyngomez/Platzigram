5013587edf4356994a55800255885e1d666add6e
Martin Gomez <martyngomez22@gmail.com>
2018-01-18 16:43:13 -0300

    Subir Foto a Firebase Storage.

--- app/src/main/java/com/martyngomez/platzigram/PlatzigramApplication.java
+++ app/src/main/java/com/martyngomez/platzigram/PlatzigramApplication.java
@@ -1,14 +1,24 @@
 package com.martyngomez.platzigram;
 
 import android.app.Application;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.facebook.FacebookSdk;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 /**
  * Created by MTN on 12/01/2018.
  */
 
 public class PlatzigramApplication extends Application {
+    private static final String TAG = "PlatzigramApplication" ;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseStorage firebaseStorage;
 
     @Override
     public void onCreate() {
@@ -16,5 +26,29 @@ public class PlatzigramApplication extends Application {
 
         FacebookSdk.sdkInitialize(getApplicationContext());
 
+
+        firebaseAuth = FirebaseAuth.getInstance(); // Obtiene la informacion desde json
+        authStateListener = new FirebaseAuth.AuthStateListener() { //Listener que esta pendiente de lo cambios en la sesion
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {  //Si la ssion cambia ejecuta el codigo
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null){ //Valida si esta logueado
+                    Log.w(TAG, "Usuario Logueado" + firebaseUser.getEmail());
+                }
+                else{
+                    Log.w(TAG, "Usuario No Logueado" );
+                }
+            }
+        };
+
+        firebaseStorage = FirebaseStorage.getInstance();  //Inicializa Firebase Storage
+
     }
+
+
+    public StorageReference getStorageReference(){
+        return firebaseStorage.getReference(); //URL raiz del storage
+    }
+
+
 }
--- app/src/main/java/com/martyngomez/platzigram/post/view/NewPostActivity.java
+++ app/src/main/java/com/martyngomez/platzigram/post/view/NewPostActivity.java
@@ -1,24 +1,95 @@
 package com.martyngomez.platzigram.post.view;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.martyngomez.platzigram.PlatzigramApplication;
 import com.martyngomez.platzigram.R;
 import com.squareup.picasso.Picasso;
 
+import java.io.ByteArrayOutputStream;
+
 public class NewPostActivity extends AppCompatActivity {
+    private static final String TAG ="NewPostActivity" ;
     private ImageView imgPhoto;
+    private Button btnCreatePost;
+    private String photoPath;
+    private PlatzigramApplication app;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
+
+        app = (PlatzigramApplication) getApplicationContext(); // inicializa app para traer datos globales
+        storageReference =  app.getStorageReference();
+
         imgPhoto = (ImageView) findViewById(R.id.imgPhoto);
+        btnCreatePost = (Button) findViewById(R.id.btnCreatePost);
 
         if (getIntent().getExtras()!= null){
-            String photoPath = getIntent().getExtras().getString("PHOTO_PATH_TEMP"); //Obtiene parametro
-            Picasso.with(this).load(photoPath).into(imgPhoto); //Carga la foto
+            photoPath = getIntent().getExtras().getString("PHOTO_PATH_TEMP"); //Obtiene parametro
+            showPhoto();
         }
+
+        btnCreatePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadPhoto();
+            }
+        });
+    }
+
+    private void uploadPhoto() {
+        imgPhoto.setDrawingCacheEnabled(true);
+        imgPhoto.buildDrawingCache(); //Genera un cache de la foto
+
+        Bitmap bitmap = imgPhoto.getDrawingCache(); //Pasa la foto del cache a un Bitmap
+        ByteArrayOutputStream baos =  new ByteArrayOutputStream(); //Se utiliza para la escritura en Firebase
+
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,baos); // Comprime la foto
+
+        byte[] photoByte = baos.toByteArray(); // Asigna foto a bytes
+
+        String photoName = photoPath.substring(photoPath.lastIndexOf("/")+1,photoPath.length()); //Corta en nombre de la foto
+
+        StorageReference photoReference =  storageReference.child("postImages/" + photoName);
+
+        UploadTask uploadTask = photoReference.putBytes(photoByte); // sube la imagen , putfile es para trabajar con archivos
+        uploadTask.addOnFailureListener(new OnFailureListener() { //Listener fallo
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() { //istner exito
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Uri uriPhoto = taskSnapshot.getDownloadUrl();
+                String photoURL = uriPhoto.toString();
+                Log.w(TAG, "URL Photo = " + photoURL);
+                finish();
+            }
+        });
+
+
+
+
+
+    }
+
+    private void showPhoto() {
+        Picasso.with(this).load(photoPath).into(imgPhoto); //Carga la foto
     }
 }
